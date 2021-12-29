850ea815dc824fe67f72f8bf2316e51ef98cc036
Marrowsed <marcus.ah@gmail.com>
2021-12-28 21:21:09 -0300

    Main atualizado

--- main.py
+++ main.py
@@ -1,23 +1,25 @@
 from Contas import *
 from Pessoa import *
+from Poupanca import *
 
 print("Relatório de Contas")
 cliente = Pessoa("Marcus")
 print(f"Olá, {cliente.nome} !")
 banco = input("Digite o nome do Banco: ")
-contas = Contas(cliente, banco)
+poupanca = Poupanca()
+contas = Contas(cliente, banco, poupanca)
 contas.balancos()
 
 count = 1
 
 
 def retorna():
-    banco = input("Digite o nome do Banco: ")
-    contas2 = Contas(cliente, banco)
+    banco2 = input("Digite o nome do Banco: ")
+    contas2 = Contas(cliente, banco2, poupanca)
     contas2.balancos()
     i = 1
     while i != 0:
-        print(f"Banco: {contas2._banco}")
+        print(f"Banco: {contas2.banco}")
         print("---------------------------------------")
         print("1 - Ações dentro da Conta Corrente")
         print("---------------------------------------")
@@ -27,21 +29,20 @@ def retorna():
         print("---------------------------------------")
         print("4 - Finalizar")
         print("---------------------------------------")
-        escolha = int(input("Digite o número: "))
-        if escolha == 1:
+        escolha2 = int(input("Digite o número: "))
+        if escolha2 == 1:
             contas2.conta_corrente()
-        elif escolha == 2:
+        elif escolha2 == 2:
             contas2.conta_credito()
-        elif escolha == 3:
-            contas.menu()
+        elif escolha2 == 3:
+            contas2.poupanca.menu()
         else:
             print("Término")
-            i = i -1
-
+            i = i - 1
 
 
 while count != 0:
-    print(f"Banco: {contas._banco}")
+    print(f"Banco: {contas.banco}")
     print("---------------------------------------")
     print("1 - Ações dentro da Conta Corrente")
     print("---------------------------------------")
@@ -59,7 +60,7 @@ while count != 0:
     elif escolha == 2:
         contas.conta_credito()
     elif escolha == 3:
-        contas.menu()
+        contas.poupanca.menu()
     elif escolha == 4:
         break
     else:
